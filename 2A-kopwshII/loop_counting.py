a9d0394446797e9e8d6f04b6e0cffead2baa0040
Vasileios Papamichail <bpapamichael4@gmail.com>
2025-01-17 13:48:32 +0200

    remove double loops when loop counting and add elapsed time

--- 2A-kopwshII/loop_counting.py
+++ 2A-kopwshII/loop_counting.py
@@ -2,29 +2,40 @@ import strain_life_utils as sl
 import numpy as np
 from matplotlib import pyplot as plt
 import utilities as utils
+import time
+start_time = time.time()
 
+# parameters
 E = 210000
 Ku = 1434
 nu=0.14
 Kt = 1.574
 s_normalization = 174
-factor = 100
-sequence = utils.reader("Strain-Gauge-Console")
-sequence = np.array(sequence[:22])*s_normalization/100 * Kt * factor #notch
-# print(sequence)
+factor = 1 # scale
 
+# sequence read
+sequence = utils.reader("Strain-Gauge-Console")
+sequence = np.array(sequence[:30])*s_normalization/100 * factor #notch
+# print(sequence)
 # sequence = np.array([0.7, 0.3, 0.65, 0.2, 0.82, 0.5, 0.8,0.3, 1, 0.4])*s_normalization*factor
-err_e = 1e-3 # tune based on the factor
+
+# def errors for loop counting
+err_e = 1e-5 # tune based on maximum stress
 err_s = 1e-2
+
 stress = []
 strain = []
-branches = []
+
 plt.figure(figsize=(16, 9))
+
+# initial R-O
 stress.append(sl.solve_Neuber(0,0,sequence[0],Kt,E,Ku,nu,'R-O'))
 strain.append(sl.ramberg_osgood(stress[0],E,Ku,nu))
 # print(stress[0])
 sl.ramberg_osgood_plot(stress[0],E,Ku,nu)
 # sl.ramberg_osgood_plot(np.max(sequence), E,Ku,nu)
+
+# initialize limit for Memory type 1
 s_ro = np.linspace(0,(np.max(sequence)),100*factor)
 e_ro = s_ro/E + (s_ro/Ku)**(1/nu)
 s_c = 0
@@ -83,10 +94,13 @@ for i,S in enumerate(sequence[1:]):
     strain.append(e)
     s_mas.append(smas)
     e_mas.append(emas)
+
+
+# improved rainflow
 loops = 0
 loop = []
 for j in range(1, len(s_mas)+1):
-    # Extract the min/max points for the (j-1)-th Masing curve
+
     min_s = np.min(s_mas[j - 1]) if e_mas[j - 1][0] - e_mas[j - 1][1] < 0 else np.max(s_mas[j - 1])
     max_s = np.max(s_mas[j - 1]) if e_mas[j - 1][0] - e_mas[j - 1][1] < 0 else np.min(s_mas[j - 1])
     min_e = np.min(e_mas[j - 1]) if e_mas[j - 1][0] - e_mas[j - 1][1] < 0 else np.max(e_mas[j - 1])
@@ -97,7 +111,8 @@ for j in range(1, len(s_mas)+1):
     max_pair = np.array((max_s, max_e))
 
     dir_excl = "d" if s_mas[j-1][0]-s_mas[j-1][1] > 0 else "u"
-    # Loop through other Masing curves (excluding the current one)
+
+
     for k in range(len(s_mas)):
         if k == j - 1:  # Skip the current (j-1)-th Masing curve
             continue
@@ -115,14 +130,14 @@ for j in range(1, len(s_mas)+1):
         ch2 = np.isclose(ch_temp[:, 0], max_pair[0], err_s) & np.isclose(ch_temp[:, 1], max_pair[1],err_e)
 
         # print(ch1)
-        # Debugging: Print the comparison results for inspection
+        # DEBUG: Print the comparison results for inspection
         # print(f"Checking curve {k} against curve {j - 1}")
         # print(dir_temp)
         # print(dir_excl)
         # print(f"Min Pair: {min_pair}, Max Pair: {max_pair}")
         # print(f"ch1: {np.any(ch1)}, ch2: {np.any(ch2)}")
 
-        # If all conditions are satisfied, print the index of the intersecting curve
+
         if np.any(ch1) == 1 and np.any(ch2) == 1:
             if dir_temp != dir_excl:
                 if k > j-1:
@@ -147,24 +162,49 @@ for j in range(1, len(s_mas)+1):
                         continue
             else:
                 continue
+# print(len(loop))
 
-####### REMOVE DOUBLE LOOPS. EG. 10 AND 11 IN LOOP
+# REMOVE DOUBLE LOOPS
+for z in range(len(loop) - 2, -1, -1):  # Iterate in reverse
+    if (
+        loop[z][0] == loop[z+1][0] - 1
+        and loop[z][1] == loop[z+1][3]
+        and loop[z][2] == loop[z+1][4]
+        and loop[z][3] == loop[z+1][1]
+        and loop[z][4] == loop[z+1][2]
+    ):
+        del loop[z+1]
 
-# ar = [1.00001e10 ,1e10]
-# ar2 = [2, 2.00000001]
-# arr = np.column_stack((ar, ar2))
+# print(len(loop))
 
-#     print("Yes")
-# c1 = np.isclose([1e10, 2], arr)
-# c2 = np.isclose([1.000001e10, 3.000001], [1.000001e10, 3])
-# if np.all(c1) == 1 and np.all(c2) == 1:
-#     print("yes")
-# else:
-#     print("no")
-# print(c1)
+
+
+
+
+
+
+
+
+
+
+
+total_loops = len(loop)
+print(total_loops)
+
 plt.grid()
 plt.show()
-print(loops)
+
 print(loop)
-# print(s_mas[1])
-# print(stress[-1], strain[-1], stress[-2], strain[-2])
+
+
+
+
+
+
+
+
+
+
+
+
+print("time elapsed: {:.2f}s".format(time.time() - start_time))
