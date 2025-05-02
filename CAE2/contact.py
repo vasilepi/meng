e7d6f2f3186d713b7f5ef81fed350bb24ec9b3b0
Vasileios Papamichail <bpapamichael4@gmail.com>
2025-05-02 21:45:12 +0300

    add hertz linear theory

--- CAE2/contact.py
+++ CAE2/contact.py
@@ -1,1 +1,58 @@
 # SCRIPT FOR 2nd assignement on CAE2
+
+##### PART B THEORETICAL
+### HERTZ LINEAR
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+r1 = 5 # mm
+r2 = 5 # mm
+l = 10 # mm
+f = 10000 # N
+
+n1 = 0.3
+n2 = 0.3
+E1 = 210000 # MPa
+E2 = 210000 # MPa
+
+
+E_star = 1/((1-n1**2)/E1 + (1-n2**2)/E2)
+R_star = 1/(1/r1 + 1/r2)
+
+# print(E_star, R_star)
+alin = np.sqrt(4*f*R_star/np.pi/E_star/l)
+# print(alin)
+
+polin = 2*f/np.pi/alin/l
+xlin = np.linspace(-alin, alin)
+plin = polin * np.sqrt(1-(xlin/alin)**2)
+
+ulin = np.linspace(0, 10)
+Flin = np.pi/4 * E_star*l * ulin
+
+zlin = np.linspace(-10,10)
+slinx1 = -2* n1 * polin * (np.sqrt(1+(zlin/alin)**2)- np.abs(zlin/alin))
+slinx2 = -2* n2 * polin * (np.sqrt(1+(zlin/alin)**2)- np.abs(zlin/alin))
+sliny = -polin * ((1+2*(zlin/alin)**2)/np.sqrt(1+(zlin/alin)**2) - 2* np.abs(zlin/alin))
+slinz = -polin/np.sqrt(1+(zlin/alin)**2)
+
+
+
+# plot
+plt.figure(figsize=(9,5))
+plt.grid(True, which='both')
+plt.plot(xlin, plin)
+plt.legend()
+plt.xlabel(r"$x\; (mm)$")
+plt.ylabel(r"$P\; (MPa)$")
+plt.show()
+
+plt.figure(figsize=(9,5))
+plt.grid(True, which='both')
+plt.plot(ulin, Flin)
+plt.legend()
+plt.xlabel(r"$u\; (mm)$")
+plt.ylabel(r"$F\; (N)$")
+plt.show()
+
